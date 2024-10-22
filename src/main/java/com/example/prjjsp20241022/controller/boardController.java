c64b7b6d16807c5140226033b993a147bc0b0591
seokminlee24 <lsm67286564@gmail.com>
2024-10-22 10:26:46 +0900

    새 게시물 쓰기

--- src/main/java/com/example/prjjsp20241022/controller/boardController.java
+++ src/main/java/com/example/prjjsp20241022/controller/boardController.java
@@ -1,14 +1,20 @@
 package com.example.prjjsp20241022.controller;
 
+import com.example.prjjsp20241022.dto.Board;
+import com.example.prjjsp20241022.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class boardController {
+
+    private final BoardService service;
+
     // 게시물 CRUD
 
     // /board/new
@@ -16,6 +22,11 @@ public class boardController {
     public void newBoard() {
 
         // /WEB-INF/view/board/new.jsp
+    }
 
+    @PostMapping("new")
+    public void newBoard(Board board) {
+
+        service.add(board);
     }
 }
