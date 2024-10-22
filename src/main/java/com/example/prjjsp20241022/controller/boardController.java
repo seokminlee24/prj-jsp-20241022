867fce8f271a3ec1322370b6fbf44b027debf4d8
seokminlee24 <lsm67286564@gmail.com>
2024-10-22 09:51:08 +0900

    새 게시물 쓰기 Controller,jsp

---
+++ src/main/java/com/example/prjjsp20241022/controller/boardController.java
@@ -0,0 +1,21 @@
+package com.example.prjjsp20241022.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class boardController {
+    // 게시물 CRUD
+
+    // /board/new
+    @GetMapping("new")
+    public void newBoard() {
+
+        // /WEB-INF/view/board/new.jsp
+
+    }
+}
