c7495a9e03b34e8d66fe36372c3e5c550ecd75e7
seokminlee24 <lsm67286564@gmail.com>
2024-10-24 15:34:02 +0900

    회원 정보 보기

--- src/main/java/com/example/prjjsp20241022/mapper/MemberMapper.java
+++ src/main/java/com/example/prjjsp20241022/mapper/MemberMapper.java
@@ -3,6 +3,9 @@ package com.example.prjjsp20241022.mapper;
 import com.example.prjjsp20241022.dto.Member;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface MemberMapper {
@@ -12,4 +15,17 @@ public interface MemberMapper {
             VALUES (#{id}, #{password}, #{nickName}, #{description})
             """)
     int insert(Member member);
+
+    @Select("""
+            SELECT *
+            FROM member
+            ORDER BY id 
+            """)
+    List<Member> selectAll();
+
+    @Select("""
+            SELECT * FROM member
+            WHERE id = #{id}
+            """)
+    Member selectById(String id);
 }
